5e9b88f62b3d2c9eab7d96f8ae092c6997e4b304
עמית יפה < >
2016-04-23 23:42:26 +0300

    adding race course descriptor

--- app/src/main/java/com/aayaffe/sailingracecoursemanager/Racecourse/RaceCourseDescriptor.java
+++ app/src/main/java/com/aayaffe/sailingracecoursemanager/Racecourse/RaceCourseDescriptor.java
@@ -1,8 +1,13 @@
 package com.aayaffe.sailingracecoursemanager.Racecourse;
 
+import java.util.List;
+
 /**
  * Created by aayaffe on 22/04/2016.
  */
 public class RaceCourseDescriptor {
     //TODO add a descriptor of race course (bearing distance)
+    List<RaceCourseObject> objects;
+
+    
 }
