6f3cdfdf4366e18ff16a7b33189497c4654e2420
Amit Yaffe <aayaffe@gmail.com>
2023-05-06 20:58:50 +0300

    Fixed issue with map renderer

--- app/src/main/java/com/aayaffe/sailingracecoursemanager/general/HSApplication.java
+++ app/src/main/java/com/aayaffe/sailingracecoursemanager/general/HSApplication.java
@@ -1,14 +1,19 @@
 package com.aayaffe.sailingracecoursemanager.general;
 
+
 import android.content.Context;
+import android.util.Log;
+
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
 
 import com.aayaffe.sailingracecoursemanager.R;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
 //import com.tenmiles.helpstack.HSHelpStack;
 //import com.tenmiles.helpstack.gears.HSEmailGear;
 
-public class HSApplication extends MultiDexApplication {
+public class HSApplication extends MultiDexApplication implements OnMapsSdkInitializedCallback {
 //	HSHelpStack helpStack;
 
 	private static HSApplication instance;
@@ -33,6 +38,18 @@ public class HSApplication extends MultiDexApplication {
 //		helpStack.setGear(emailGear); // Set the Gear
 		instance = this;
 		MultiDex.install(this);
+		MapsInitializer.initialize(getApplicationContext(), MapsInitializer.Renderer.LATEST, this);
 
+	}
+	@Override
+	public void onMapsSdkInitialized(MapsInitializer.Renderer renderer) {
+		switch (renderer) {
+			case LATEST:
+				Log.d("HSAApplication", "The latest version of the renderer is used.");
+				break;
+			case LEGACY:
+				Log.d("HSAApplication", "The legacy version of the renderer is used.");
+				break;
+		}
 	}
 }
